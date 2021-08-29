2d86191a52e2b0cc7c5c5f620ddbc27c63e01aeb
Harry Klein <harry@joomla-hklein.de>
2021-08-30 00:59:40 +0200

    Code aufgeräumt

--- src/CanControl.h
+++ src/CanControl.h
@@ -1,29 +1,7 @@
 #ifndef CAN_CONTROL_H
 #define CAN_CONTROL_H
 
-
-#define ID_STATUS 400
-#define ID_SET 200
-
-#define MSB_IS_MODUL 0xFF
-
-#define COMMAND_CONFIG 0x03
-
-#define AKTOR_WEICHE 0x01
-#define AKTOR_HP 0x02
-#define AKTOR_VERSION 0x80
-
-#define INDEX_COMMAND 0x00
-#define INDEX_MSB 0x01
-#define INDEX_LSB 0x02
-#define INDEX_AKTOR 0x03
-#define INDEX_DATA0 0x04
-#define INDEX_DATA1 0x05
-#define INDEX_DATA2 0x06
-#define INDEX_DATA3 0x07
-
 #include <ACAN2515.h>
-//#include <SPI.h>
 
 static const byte MCP2515_CS = 10;  // CS input of MCP2515
 static const byte MCP2515_INT = 3;  // INT output of MCP2515
@@ -34,8 +12,10 @@ static const uint32_t QUARTZ_FREQUENCY = 16UL * 1000UL * 1000UL;  // 16 MHz
 
 void init_can() {
   SPI.begin();
-  ACAN2515Settings settings(QUARTZ_FREQUENCY, 125UL * 1000UL);  // CAN bit rate 125 kb/
-  settings.mRequestedMode = ACAN2515Settings::NormalMode;     // Select loopback mode
+  ACAN2515Settings settings(QUARTZ_FREQUENCY, 125UL * 1000UL);
+  settings.mRequestedMode = ACAN2515Settings::NormalMode;
+  /** Buffer verkleinert, da wenig RAM verfügbar ist. Siehe auch 
+   * https://github.com/pierremolinaro/acan2515/issues/2 */
   settings.mReceiveBufferSize = 16 ;
   settings.mTransmitBuffer0Size = 8 ;
   const uint16_t errorCode = can.begin(settings, [] { can.isr(); });
